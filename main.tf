ac80681917de34dd5dafa71c5cffacd5b079c3bd
sara-wq <52618907+sara-wq@users.noreply.github.com>
2024-09-18 22:39:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,6 +41,17 @@ resource"aws_instance" "my_first_instance"{
     }
   }
 
+resource "aws_s3_bucket" "my-s3-bucket"{
+  bucket_prefix=var.bucket_prefix
+  acl=var.acl
+
+  versioning{
+    enabled=var.versioning
+  }
+  tags=var.tags
+}
+    
+
   
 
 
