76ce5fa0b0897bc6f795c992f7277e97570bf813
sara-wq <52618907+sara-wq@users.noreply.github.com>
2024-09-18 12:24:20 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "my_security_grp" {
+  name= var.security_grp
+
+ ingress {
+  from_port = 8080
+  to_port = 8080
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ingress {
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ egress {
+  from_port = 0
+  to_port = 65535
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+
+tags={
+  name=var.security_grp
+  }
+}
+
+resource"aws_instance" "my_first_instance"{
+  ami = var.ami
+  
+
+
+
+
